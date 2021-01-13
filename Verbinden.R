1eae98ccb6210551dd44016df1b9aca09f5cda11
lisaalisaa <71631424+lisaalisaa@users.noreply.github.com>
2021-01-13 14:41:07 +0100

    manifest Datei erstellen

--- Verbinden.R
+++ Verbinden.R
@@ -1,13 +1,15 @@
 
-
+#install.packages("rsconnect")
 
 # In a directory with app.R
-list.files()
-# [1] "app.R"
+list.files(path = "test")
+
 
 # Create the manifest
-rsconnect::writeManifest()
+rsconnect::writeManifest(appDir = "test")
 
 # Confirm manifest.json output
-list.files()
-# [1] "app.R" "manifest.json"
+list.files(path = "test")
+
+
+
