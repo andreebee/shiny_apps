cf12afcb69dab7859bd6b1ce6e1a2902fd943975
bezbezi <35496755+bezbezi@users.noreply.github.com>
2021-12-13 08:53:18 +0100

    UI Translator

--- test_behzad_git/app.R
+++ test_behzad_git/app.R
@@ -9,18 +9,35 @@
 
 library(shiny)
 library(tidyverse)
+library(R.utils)
+library(shiny.i18n)
+
+i18n <- Translator$new(translation_json_path = "https://raw.githubusercontent.com/bezbezi/vanil/master/translation.json") 
+
+#i18n$set_translation_language("en")
 
 # Define UI for application that draws two normal distributions using given mean and standard deviation
 ui <- fluidPage(
+    selectInput("language","please select language",c("en","de")),
 
-    # Application title
-    titlePanel("Compare 2 Normal Distributions"),
+    conditionalPanel(
+      condition = "input.language == 'en'",
+      conditionalPanel('false', i18n$set_translation_language("en")),
+      titlePanel(i18n$t("Compare 2 normal distributions"))
+    ),
+    
+    conditionalPanel(
+      condition = "input.language == 'de'",
+      conditionalPanel('false', i18n$set_translation_language("de")),
+      titlePanel(i18n$t("Compare 2 normal distributions"))
+    ),
+    
 
     sidebarLayout(
         # For getting the mean and std of 2 variables
         # and specifying the alpha to shade the alpha and 1-beta in the graph
         sidebarPanel(
-            numericInput("mean1","Average of var1",value=-2.5),
+            numericInput("mean1",i18n$t("Average of var1"),value=-2.5),
             numericInput("sd1","Standard Deviation of var1",value=3,min=0),
             numericInput("mean2","Average of var2",value=2),
             numericInput("sd2","Standard Deviation of var2",value=3.5,min=0),
