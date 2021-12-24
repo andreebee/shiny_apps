a88878d3e16fe4cdc71c070e2466b107d77c73ba
bezbezi <35496755+bezbezi@users.noreply.github.com>
2021-12-24 10:53:07 +0100

    add a readme file and comments

--- i18n-app-test/server.R
+++ i18n-app-test/server.R
@@ -1,3 +1,4 @@
+# load packages and global.r for translation
 library(shiny)
 library(R.utils)
 source("../global.R")
@@ -6,13 +7,16 @@ server <- function(input, output, session) {
   i18n <-languageServer('langSelect', "./translations.json")
 
   output$page_content <- renderUI({
+    # using taglist to add multiple ui input and elements
     tagList(
+      # using i18n()$t to translate the text
       p(i18n()$t("msg_HiThere")),
       p(i18n()$t("msg_Fruits"))
     )
   })
   
   output$hi <- renderText({
+    # using i18n()$t to translate the text
     i18n()$t("msg_HelloWorld")
   })
 }
--- i18n-app-test/ui.R
+++ i18n-app-test/ui.R
@@ -1,9 +1,13 @@
+# load packages and global.r for translation
 library(shiny)
 library(R.utils)
 source("../global.R")
 
 ui <- fluidPage(
+  # adding a selector to choose the app language
   languageSelector("langSelect"),
+  # using uiOutput to translate ui input elements
   uiOutput('page_content'),
+  # adding a sample output elements
   textOutput("hi")
 )
