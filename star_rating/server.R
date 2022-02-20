6194e931611a9b3338052b3e5656795060ca6e45
bezbezi <35496755+bezbezi@users.noreply.github.com>
2022-02-20 19:22:32 +0100

    a test app to rate the app

---
+++ star_rating/server.R
@@ -0,0 +1,78 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define the fields we want to save from the form
+fields <- c("name","n_stars")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+
+  # to show the stars
+  output$stars_ui <- renderUI({
+    # to calculate our input %
+    n_fill <- (input$n_stars / 5) * 100
+    # element will look like this: <div class="full-stars" style="width:n%"></div>
+    style_value <- sprintf("width:%s%%", n_fill)
+    tags$div(class = "full-stars", style = style_value)
+  })
+  
+  # to save data
+  saveData <- function(data) {
+    data <- as.data.frame(t(data))
+    if (exists("responses")) {
+      responses <<- rbind(responses, data)
+    } else {
+      responses <<- data
+    }
+  }
+  
+  # to load data
+  loadData <- function() {
+    if (exists("responses")) {
+      datatable(data.frame(
+        responses,
+        row.names = session$request$REMOTE_ADDR
+        ),
+        # to remove search bar
+        options = list(dom = 't'))
+    }
+  }
+  
+  # Whenever a field is filled, aggregate all form data
+  formData <- reactive({
+    data <- sapply(fields, function(x) input[[x]])
+    data
+  })
+  
+  # When the Submit button is clicked, save the form data
+  observeEvent(input$submit, {
+    saveData(formData())
+  })
+  
+  # Show the previous responses
+  # (update with current response when Submit is clicked)
+  output$responses <- DT::renderDataTable({
+    input$submit
+    loadData()
+  })
+
+})
---
+++ star_rating/ui.R
@@ -0,0 +1,54 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      
+      # name
+      textInput("name","Name"),
+      
+      # rate the app
+      includeCSS("www/stars.css"),
+      sliderInput(inputId = "n_stars", label = "Ratings", min = 0,  max = 5, value = 3, step = 0.5),
+      tags$div(class = "ratings",
+               tags$div(class = "empty-stars",
+                        uiOutput("stars_ui")
+               )
+      ),
+      # to save the rating
+      actionButton("submit", "Submit")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot"),
+      DT::dataTableOutput("responses", width = 300), tags$hr()
+    )
+  )
+  
+  
+    
+  
+))
